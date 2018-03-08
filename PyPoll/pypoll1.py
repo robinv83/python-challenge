62001cbcc53cc803dede1c143f5e78db734eb8bf
Robin Vasudevan <robinv83@gmail.com>
2018-03-08 17:12:27 -0500

    Add files via upload

--- PyPoll/pypoll1.py
+++ PyPoll/pypoll1.py
@@ -1,17 +1,41 @@
 import os
 import csv
+from collections import Counter
 
 csvpath = os.path.join(r"C:\Users\Admin\Desktop\UNCCHAR201802DATA2-Class-Repository-DATA\Homework\Week-03-Python\Instructions\PyPoll\raw_data\election_data_1.csv")
 
 
 row_count = 0
-mydict = {}
+voting_info= []
 with open(csvpath, newline= "") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ',')
     #next(csv_reader)
-    mydict = {rows[0]: rows[2] for rows in csv_reader}
-    for rows in mydict:
-        print(rows, mydict[rows])
+    for row in csv_reader:
+        voting_info = [[row[0], row[1], row[2]] for row in csv_reader]
+        counter = len(list(voting_info)) # Number of votes
+        row_count = counter
+        print(row_count)
+
+
+candidate_count = 0
+candidate_info = []
+with open(csvpath, newline= "") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    #next(csv_reader)
+    for row in csv_reader:
+        candidate_info = [[row[2]] for row in csv_reader]
+        unique_data = [list(row) for row in set(tuple(row) for row in candidate_info)]
+        cntr = len(list(unique_data)) # Number of candidates
+        candidate_count = cntr
+        print(candidate_count)
+
+
+
+
+
+
+
+
 
 
 
